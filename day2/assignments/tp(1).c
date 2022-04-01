81dfcb2f3ba6e5629ac2ef263fcaa4521f92e073
ZouhairiSalma <92038490+zouhairisalma@users.noreply.github.com>
2022-04-01 09:44:03 +0000

    Add files via upload

---
+++ day2/assignments/tp(1).c
@@ -0,0 +1,65 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+float a,b,c;
+printf("donner a");
+scanf("%f",&a);
+printf("donner b");
+scanf("%f",&b);
+c=a*b;
+printf("%.3f",c);
+return 0;
+}
+
+
+/*{
+#include <stdio.h>
+
+int main()
+{
+int   i =  8;
+int   j =  5;
+float x =  0.005f;
+float y = -0.01f;
+char  c = 'c';
+char  d = 'd';
+a=(3i - 2j)%(2*d - c)
+printf("(3i - 2j)%(2*d - c) n'est pas calculable");
+printf("2*((i/5) + (4*(j-3))%(i + j - 2))=%f",2*((i/5) + (4*(j-3))%(i + j - 2)));
+printf("i<=j%s",i<=j ? "true":"false");
+printf("j != 6%s",j != 6 ?"true":"false");
+printf("c == 99%s",c == 99 ?"true":"false");
+printf(" "5*(i + j) > 'c'" n'a pas de valeur");
+printf("(i > 0) && (j < 5)%s",(i > 0) && (j < 5) ? "true":"false");
+printf("(x > y) && (i > 0) || (j < 5)%s",(x > y) && (i > 0) || (j < 5) ? "true":"false");
+printf("(x > y) && (i > 0) && (j < 5)%s",(x > y) && (i > 0) && (j < 5) ? "true":"false");
+return 0;
+}
+
+
+
+
+
+#include <stdio.h>
+
+int main()
+{
+
+int a,b,c;
+printf("donner a");
+scanf("%f",&a);
+printf("donner b");
+scanf("%f",&b);
+c=b;
+b=a;
+a=c;
+
+printf("les valeurs de a et b resp. : %d%d\n",a,b)
+return 0;
+}
+
+}*/
+
